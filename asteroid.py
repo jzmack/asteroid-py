0a901c3d182c5d780b1e7c573062029ea483b405
jzmack <canesfanjake1@gmail.com>
2025-07-15 15:34:19 -0400

    added asteroid splitting logic so that asteriods get split when hit (if they are big enough)

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import CircleShape
 import pygame
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,18 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
         
+        split_angle = random.uniform(20, 50)
+        split_asteroid_a = self.velocity.rotate(split_angle)
+        split_asteroid_b = self.velocity.rotate(-split_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = split_asteroid_a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = split_asteroid_b * 1.2
--- main.py
+++ main.py
@@ -37,6 +37,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill(color="black")
         
