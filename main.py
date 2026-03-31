0e09fc2f6d43c50bf747df4243e37efe137b3486
Jake Mackin <canesfanjake1@gmail.com>
2026-03-31 12:32:46 -0400

    added logger.py, changed main.py to print info when ran

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ from asteroidfield import *
 from shot import *
 
 def main():
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
     asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    
+
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
@@ -43,12 +47,12 @@ def main():
                     asteroid.split()
 
         screen.fill(color="black")
-        
-        
+
+
         for thing in drawable:
             thing.draw(screen)
         dt = clock.tick(60) / 1000
-        
+
         pygame.display.flip()
 
 if __name__ == "__main__":
